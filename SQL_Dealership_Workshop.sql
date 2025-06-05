7855c51839cdb4ac8fbeb06468f8ad87f73949a2
sarahfarris <sarahfarris45@gmail.com>
2025-06-05 15:06:21 -0700

    Finished tables

--- SQL_Dealership_Workshop.sql
+++ SQL_Dealership_Workshop.sql
@@ -14,9 +14,9 @@ SOLD boolean);
 
 CREATE TABLE inventory (
 dealership_id int NOT NULL,
-VIN varchar(20) NOT NULL
+VIN varchar(20) NOT NULL,
 -- Google recommended to define unique constraint to ensure VIN is only once per dealership's inventory (?)
-UNIQUE (dealership_id, VIN) ,
+UNIQUE (dealership_id, VIN),
 -- Defining foreign key constraints (?)
 FOREIGN KEY (dealership_id) REFERENCES dealerships(dealership_id),
 FOREIGN KEY (VIN) REFERENCES vehicles(VIN)
